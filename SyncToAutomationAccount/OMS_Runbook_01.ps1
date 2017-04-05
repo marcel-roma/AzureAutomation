4c9aae38f0462c42575cbe6d70308a05c58b340a
Marcel Roma <marcel.roma@soft-xpress.de>
2017-04-05 18:03:52 +0200

    Updated OMS_Runbook_01.ps1 in Azure Automation.

--- SyncToAutomationAccount/OMS_Runbook_01.ps1
+++ SyncToAutomationAccount/OMS_Runbook_01.ps1
@@ -1,14 +1,19 @@
 Write-Output "Script execution begins here...";
 
+Write-Output "Getting connection...";
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection;
 
+Write-Output "Logging on...";
 $null = Add-AzureRMAccount `
                 -ServicePrincipal `
                 -Tenant $Conn.TenantID `
                 -ApplicationId $Conn.ApplicationID `
                 -CertificateThumbprint $Conn.CertificateThumbprint;
 
+Write-Output "Getting resource groups..."
 $resourceGroups = Get-AzureRmResourceGroup;
 
 Write-Output "Output:";
 Write-Output $resourceGroups;
+
+Write-Output "Script ends here.";
