e296029432fcc60c5ba9007427eff762a2a895c8
Marcel Roma <marcel.roma@soft-xpress.de>
2017-06-01 11:32:30 +0200

    Update Configure-OneDrive.ps1

--- SyncToAutomationAccount/Configure-OneDrive.ps1
+++ SyncToAutomationAccount/Configure-OneDrive.ps1
@@ -10,7 +10,7 @@ Import-Module O365_Functions;
 
 Connect-SharePointTenant -stage $stage;
 
-Set-SPOTenantSyncClientRestriction -BlockMacSync:$false -DomainGuids "1126dc51-d9d5-4527-989b-635a682a134f; 2e37d61e-5442-446d-b45f-4e872bb21998; 9fe8c4c1-f64f-425f-bbf3-eed1fd4c96a5" -Enable;
+Set-SPOTenantSyncClientRestriction -BlockMacSync:$false -DomainGuids *DELETED* -Enable;
 Set-SPOTenantSyncClientRestriction -ExcludedFileExtensions "";
 Set-SPOTenantSyncClientRestriction -DisableReportProblemDialog:$true;
 Set-SPOTenantSyncClientRestriction -GrooveBlockOption "HardOptIn";
